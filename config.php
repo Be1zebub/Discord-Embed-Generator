2a6e465f361b9c7ba3d05768b0a596c5baa41d8e
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-05-26 23:48:16 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+	$CONFIG = Array(
+		"is not configured" => true, // enter false if you are done configuring
+
+		// you need do set your bot credentials from https://discord.com/developers/applications
+		// select ur app, goto OAuth2 tab then copy&paste CLIENT ID and CLIENT SECRET into it
+		"client_id" 	=> "XXX",
+		"client_secret" => "YYY",
+	);
+?>
