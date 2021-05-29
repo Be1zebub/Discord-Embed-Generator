4379bf68fc2c1e508200f435c567c5934fbbea14
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-05-29 22:58:21 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="https://incredible-gmod.ru/assets/inc16icon.png">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="bootstrap_superhero.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 
     <?php
@@ -49,7 +50,7 @@
 
                 <div class="form-group form-inline">
                     <input type="text" class="form-control" id="InputTitle" placeholder="Embed title">
-                    <input class="form-control" type="color" id="InputColor" colorpick-eyedropper-active="false">
+                    <input class="form-control color-picker" type="color" id="InputColor" colorpick-eyedropper-active="false">
                 </div>
             
                 <div id="TextInputParent" class="form-group">
@@ -62,6 +63,9 @@
         }
     ?>
 </body>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/monolith.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js"></script>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
